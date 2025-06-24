99f7705eec4933c6914da922fe4fd3cf11a80186
Christina Murphy <christinajun113@gmail.com>
2025-06-24 18:36:20 -0400

    updated rawText file

--- src/core/steps/rawTextIn.ts
+++ src/core/steps/rawTextIn.ts
@@ -4,5 +4,5 @@ export function extractRawText(rawText: string): Raw {
   console.log(
     `[RAW_TEXT_IN] input=<${rawText}> | output=<${rawText}> | NOTE: Accepted Transcript.`
   );
-  return rawText;
+  return rawText.trim();
 }
